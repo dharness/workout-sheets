73e0d4525a929f3cd4f72f14ab49f0e89a63504e
Dylan Harness <dharness.engineer@gmail.com>
2026-06-23 18:45:03 -0700

    Add real OAuth client ID

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // client in Google Cloud Console with this site's origin allow-listed under
 // "Authorized JavaScript origins" (e.g. https://dharness.github.io).
 const CONFIG = {
-  CLIENT_ID: "REPLACE_WITH_YOUR_CLIENT_ID.apps.googleusercontent.com",
+  CLIENT_ID: "1091663654460-8tqqbt7jgmdp59e5r6039cm5i650pre9.apps.googleusercontent.com",
   SPREADSHEET_ID: "1euI82wFBrz3iIM5rEoQvCu5ApvBcwzEXRFIsew2zqmc",
   SCOPES: "https://www.googleapis.com/auth/spreadsheets",
 };
